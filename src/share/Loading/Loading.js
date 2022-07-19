711b2b5a378c7ca47f42a9cd4288fe657aa887bb
Mendrit Miftari <mm48357@ubt-uni.net>
2022-07-19 03:15:42 +0200

    Create and design lazy Loading style

---
+++ src/share/Loading/Loading.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import  './style.css';
+
+
+
+const Loading = () => {
+  return (
+    <div className='loading'>
+      <div className='loading-text'>
+        <h1>
+          MM
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
