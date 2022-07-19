dac0f215e0f825a6068ee154d78520387351405a
Mendrit Miftari <mm48357@ubt-uni.net>
2022-07-19 03:17:20 +0200

    Create, design and adding data for About page

---
+++ src/Pages/About/About.js
@@ -0,0 +1,110 @@
+import React from "react";
+import ColorSwitch from "../../share/colorSwitch/ColorSwitch";
+import ModeSwitch from "../../share/modeSwitch/ModeSwitch";
+import Navbar from "../../share/Navbar/Navbar";
+import "./About.css";
+import { AboutData, Skills } from "./AboutData";
+import * as moment from "moment";
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div className="About">
+      <Navbar />
+      <ColorSwitch />
+      <ModeSwitch />
+      <div className="About-data">
+        <div className="About-data-title">
+          <h1>About Me</h1>
+        </div>
+        <div className="About-data-text">
+          <h1>
+            I'm Mendrit Miftari and <span>Web Developer</span>
+          </h1>
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus eos
+            porro consequuntur iure quia deserunt, sequi repudiandae sunt, non,
+            aspernatur tempore cumque libero facere eaque? Officia vero saepe
+            aliquam facere.
+          </h4>
+        </div>
+        {AboutData.map((props) => {
+          return (
+            <div className="About-Data-info" key={props.id}>
+              <div className="About-Data-info-left">
+                <div>
+                  <h4>
+                    Birthday: <span>{props.Birthday}</span>
+                  </h4>
+                </div>
+                <div>
+                  <h4>
+                    Website: <span>{props.WebSite}</span>
+                  </h4>
+                </div>
+                <div>
+                  <h4>
+                    Degree: <span>{props.Degree}</span>
+                  </h4>
+                </div>
+                <div>
+                  <h4>
+                    City: <span>{props.City}</span>
+                  </h4>
+                </div>
+              </div>
+              <div className="About-Data-info-right">
+                <div>
+                  <h4>
+                    Age: <span>{moment().diff(props.Birthday, "years")}</span>
+                  </h4>
+                </div>
+                <div>
+                  <h4>
+                    E-mail: <span>{props.email}</span>
+                  </h4>
+                </div>
+                <div>
+                  <h4>
+                    Phone: <span>{props.phone}</span>
+                  </h4>
+                </div>
+                <div>
+                  <h4>
+                    Work: <span>{props.Work}</span>
+                  </h4>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+        <div className="about-data-btn">
+          <div className="about-data-cv">
+            <Link to="/">Download CV</Link>
+          </div>
+          <div className="about-data-hire">
+            <Link to="/Contact">Hire Me</Link>
+          </div>
+        </div>
+        <div className="about-data-Skills">
+          <h1>Skills</h1>
+          <div className="about-data-skills-container">
+            {Skills.map((props) => {
+              return (
+                <div className="about-data-skill" key={props.id}>
+                  <div className="skill-icon">{props.icon}</div>
+                  <div className="skill-info">
+                    <h2 className="skill-name">{props.name}</h2>
+                    <h4 className="skill-description">{props.description}</h4>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Pages/About/AboutData.js
@@ -0,0 +1,58 @@
+import { SiHtml5, SiCss3, SiJavascript, SiPhp, SiJava, SiMysql, SiCsharp } from 'react-icons/si'
+
+export const AboutData=[{
+    id:1,
+    Birthday:'09/20/2000',
+    WebSite:'',
+    Degree:'Student of CSE',
+    City:'Drenas/Kosovo',
+    email:'miftarimendrit@gmail.com',
+    phone:'+383-46-144-653',
+    Work :'Available'
+}]
+
+export const Skills=[
+    {
+        id:1,
+        icon:<SiHtml5/>,
+        name:"HTML",
+        description:"I have good skills in HTML with last version HTML5.",
+    },
+    {
+        id:2,
+        icon:<SiCss3/>,
+        name:"CSS",
+        description:"I have good skills in CSS with last version CSS3 and CSS pre-procesors SASS and more.",
+    },
+    {
+        id:3,
+        icon:<SiJavascript/>,
+        name:"Java Script",
+        description:"I have good skills in JS and some frameworks of JS like: React, Vue, NodeJS and more.",
+    },
+    {
+        id:4,
+        icon:<SiPhp/>,
+        name:"PHP",
+        description:"I have medium skills in PHP.",
+    },
+    {
+        id:5,
+        icon:<SiJava/>,
+        name:"Java",
+        description:"I have medium skills in Java.",
+    },
+    {
+        id:6,
+        icon:<SiMysql/>,
+        name:"SQL, MySQL",
+        description:"I have good skills in database langauage like SQL, MySQL.",
+    },
+    {
+        id:7,
+        icon:<SiCsharp/>,
+        name:"C#",
+        description:"I have medium skills in C# and with Framework like .NET.",
+    },
+    
+]
