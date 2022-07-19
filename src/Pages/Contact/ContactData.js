e050f82830c4f7feec3f4d8cdd32f06191e768a1
Mendrit Miftari <mm48357@ubt-uni.net>
2022-07-19 19:14:56 +0200

    Create, design and add data for contact page

---
+++ src/Pages/Contact/ContactData.js
@@ -0,0 +1,30 @@
+import { IoMdCall } from 'react-icons/io'
+import { MdEmail, MdLocationPin} from 'react-icons/md'
+import { GiWorld} from 'react-icons/gi'
+
+export const ContactData=[
+    {
+        id:1,
+        icon:<IoMdCall/>,
+        name:'Call me on',
+        info:'+383-46-144-653'
+    },
+    {
+        id:2,
+        icon:<MdLocationPin/>,
+        name:'From',
+        info:'Kosovo'
+    },
+    {
+        id:3,
+        icon:<MdEmail/>,
+        name:'Email',
+        info:'miftarimendrit@gmail.com'
+    },
+    {
+        id:4,
+        icon:<GiWorld/>,
+        name:'Website',
+        info:'mendritmiftari.netlify.app'
+    }
+]
